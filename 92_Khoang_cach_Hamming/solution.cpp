f4de629d673d7da4415622e04ed5a0115a42a593
kodomotachi <kodomotachi2k4@gmail.com>
2023-12-10 22:52:36 +0700

    add solution for Khoang_cach_Hamming

---
+++ 92_Khoang_cach_Hamming/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	cin.tie(0) -> sync_with_stdio(0);
+
+	// Simple solution
+
+	int a, b;
+	cin >> a >> b;
+
+	bitset<32> a_1(a), b_1(b);
+
+	int cnt = 0;
+	for (int i = 0; i < 32; i++)
+		if (a_1[i] != b_1[i])
+			++cnt;
+
+	cout << cnt;
+
+	// Magic solution
+
+	int a, b;
+	cin >> a >> b;
+	cout << __builtin_popcount(a ^ b);
+
+	return 0;
+}
